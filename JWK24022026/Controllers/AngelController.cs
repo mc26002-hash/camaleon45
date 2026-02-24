76ae9009282e4043c888627f58ecc49360acef5b
Jose Angel Mate Carias <mc26002@esfe.agape.edu.sv>
2026-02-24 11:00:58 -0600

    modificaciones

--- JWK24022026/Controllers/AngelController.cs
+++ JWK24022026/Controllers/AngelController.cs
@@ -1,11 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace JWK24022026.Controllers
+namespace JWK24022026.AppMVC.Controllers
 {
     public class AngelController : Controller
     {
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult Suma()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Suma(double num1, double num2)
+        {
+            ViewBag.Resultado = num1 + num2;
+            return View();
+        }
+
+        public IActionResult Restar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Restar(double num1, double num2)
+        {
+            ViewBag.Resultado = num1 - num2;
             return View();
         }
     }
