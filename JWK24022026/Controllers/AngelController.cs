94345e6607b19bc210d83ef8e292a40c6ba207da
Jose Angel Mate Carias <mc26002@esfe.agape.edu.sv>
2026-02-24 10:05:03 -0600

    Creacion de los archivos

---
+++ JWK24022026/Controllers/AngelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JWK24022026.Controllers
+{
+    public class AngelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
